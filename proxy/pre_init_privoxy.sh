ecce79c01fa6bbe9f9e6768449beec688ee669d8
hamphh73 <dev@hampelhh.de>
2015-09-04 21:46:07 +0200

    proxy removed

--- proxy/pre_init_privoxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# customize config
-cd /usr/local/etc/privoxy/
-sed -i.bak "s:logdir /var/log/privoxy:logdir /logs:" config
-sed -i.bak "s:logfile logfile:logfile privoxy.log:" config
-#sed -i.bak "s:#debug     1:debug     1:" config
-sed -i.bak "s/listen-address  127.0.0.1:8118/listen-address  0.0.0.0:8118/" config
-rm config.bak
--- proxy/privoxy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-umask 000
-exec  /usr/local/sbin/privoxy --no-daemon --user nobody /usr/local/etc/privoxy/config
