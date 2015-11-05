cc61990a17bbbc5d93ef42dda8ceca41c98625a7
hamphh73 <dev@hampelhh.de>
2015-11-05 21:07:31 +0100

    openhab additional_addons

--- homeautomation/init_openhab.sh
+++ homeautomation/init_openhab.sh
@@ -9,7 +9,7 @@ for dir in addons configurations contexts webapps; do
 	fi
 done
 
-for dir in workspace data; do
+for dir in workspace data additional_addons; do
 	if [ ! -d "/data/openhab/$dir/" ]; then
 		mkdir /data/openhab/$dir/
 		chown -R nobody:users /data/openhab/$dir/
@@ -20,15 +20,16 @@ done
 if [ ! -d "/logs/openhab/" ]; then
 	mkdir /logs/openhab/
 	chown -R nobody:users /logs/openhab/
-	chmod -R 777 /data/openhab/$dir
+	chmod -R 777 /logs/openhab/
 fi
 
+rm -f /data/openhab/addons/*
 if [ -f "/data/openhab/configurations/addons.list" ]; then
 	version=`cat /opt/openhab/version.txt`
-	rm -f /data/openhab/addons/*
 	while read addon; do
 		if [ $addon ]; then
 			ln -s /opt/openhab-addons/$addon-$version.jar /data/openhab/addons/
 		fi
 	done </data/openhab/configurations/addons.list
 fi
+cp -s /data/openhab/addinitonal_addons/* /data/openhab/addons/
