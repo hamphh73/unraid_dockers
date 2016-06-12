c94102bd0b5a00f5fd93584ce2a4cba99ddc360c
hamphh <dev@hampelhh.de>
2016-06-12 14:33:07 +0200

    updated logstash

--- graylog/logstash.sh
+++ graylog/logstash.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 umask 000
 
-if [ ! -f "/data/logstash.conf" ]; then
-	cp /opt/logstash/logstash.conf /data/logstash.conf
-	chown nobody:users /data/logstash.conf
-	chmod 777 /data/logstash.conf
+if [ ! -d "/data/logstash/" ]; then
+	mkdir -p /data/logstash/data
+	cp /opt/logstash/logstash.conf /data/logstash/logstash.conf
+	chown -R nobody:users /data/logstash/
+	chmod -R 777 /data/logstash/
 fi
 
 if [ ! -d "/logs/graylog/" ]; then
@@ -13,5 +14,11 @@ if [ ! -d "/logs/graylog/" ]; then
   chmod 777 /logs/graylog
 fi
 
-# Start Logstash directly (doesn't work with runit)
-/opt/logstash/bin/logstash agent –f /data/logstash.conf -l /logs/graylog/logstash.log --auto-reload
+export HOME=/data/logstash/data/
+TRACE_OPTS=
+
+if [ -n "$LOGSTASH_TRACE" ]; then
+	TRACE_OPTS=--debug
+fi
+
+exec /opt/logstash/bin/logstash agent $TRACE_OPTS --config /data/logstash/logstash.conf --auto-reload
