947b0c89dd06b02c514aa43259483f7e7dcfba31
hamphh <dev@hampelhh.de>
2016-12-16 16:51:38 +0100

    init nextcloud datadir

--- nextcloud/apache2.sh
+++ nextcloud/apache2.sh
@@ -8,5 +8,11 @@ if [ ! -d "/logs/nextcloud/apache2/" ]; then
 	chmod -R 777 /logs/nextcloud/apache2/
 fi
 
+if [ ! -d "/data/nextcloud/" ]; then
+	mkdir -p /data/nextcloud/
+	chown -R nobody:users /data/nextcloud/
+	chmod -R 777 /data/nextcloud/
+fi
+
 rm -f /var/run/apache2/apache2.pid
 exec /usr/sbin/apachectl -DFOREGROUND
