bbb7ab30fff07bd293e02467af6b35cd54afcc4b
hamphh73 <dev@hampelhh.de>
2017-06-29 22:12:53 +0200

    Updated splunk install

--- splunk/splunk.sh
+++ splunk/splunk.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+for dir in var etc; do
+	if [ ! -d "/data/$dir/" ]; then
+		mkdir -p /data/$dir/
+		chown nobody:users /data/$dir
+	fi
+done
+
+if ! cmp -s /opt/splunk/etc.org/splunk.version /data/etc/splunk.version; then
+	cp -r /opt/splunk/etc.org/* /data/etc/
+fi
+
 umask 000
 exec /sbin/setuser nobody /opt/splunk/bin/splunk start --nodaemon --accept-license
